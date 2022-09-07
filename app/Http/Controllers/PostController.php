f285a1d61e0bb2b5f1183f9b1e006821c9d3e008
armaan banoriya <armaanbanoriya@gmail.com>
2022-09-07 11:16:34 +0530

    server side pagination removed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Post::with('category')->paginate(10);
+        return Post::with('category')->get();
     }
 
     /**
