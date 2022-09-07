dd7383bf358ca8f03e108dfe50deb511e3d887d6
armaan banoriya <armaanbanoriya@gmail.com>
2022-09-07 23:22:35 +0530

    Seeder updated

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SubscribeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +24,7 @@ Route::get('post/mostRecent', [PostController::class, 'mostRecent']);
 
 Route::apiResource('category', CategoryController::class);
 Route::apiResource('post', PostController::class);
+Route::apiResource('subscribe', SubscribeController::class);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('user', [AuthController::class, 'user']);
