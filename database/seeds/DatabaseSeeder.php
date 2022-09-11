ecaa1f766dc14ed45ef1a910835978ededc8f0ce
armaan banoriya <armaanbanoriya@gmail.com>
2022-09-11 22:55:59 +0530

    database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $records=[
+                    [
+                        'name' => 'vikas',
+                        'email' => '0902ee191011@rjit.ac.in',
+                        'role' => 1,
+                        'status' => 1,
+                        'password' => bcrypt('abc123!@#')
+                    ],
+                    [
+                        'name' => 'armaan banoriya',
+                        'email' => 'armaanbanoriya@gmail.com',
+                        'role' => 0,
+                        'status' => 1,
+                        'password' => bcrypt('abc123!@#')
+                    ]
+                ];
+                foreach ($records as $record) {
+                    User::create($record);
+                }
+
     }
+
+
 }
