d99c931eb6ceef9430a7bd92c63b3991112d30be
armaan banoriya <armaanbanoriya@gmail.com>
2022-09-12 08:09:50 +0530

    meta added

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title>@yield('title', '')</title>
 
@@ -39,6 +38,7 @@
     <link rel="stylesheet" href="{{ url('admin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 
     <link href="{{ url('admin/pond/filepond.css') }}" rel="stylesheet" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 
     <style>
